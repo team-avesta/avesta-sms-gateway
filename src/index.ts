66d600e64db36016f2f13e1abb80176a18d9070a
Naresh Tank <tanknaresh7@gmail.com>
2021-08-31 15:35:04 +0530

    change export supported gateway

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 import { SmsGateway } from "./SmsGateway";
-import { TwoFactor } from './2factor/2factor';
-import { Msg91 } from './msg91/msg91';
 
 export class SmsSender {
-    constructor(private smsGateway:SmsGateway) {}
+    constructor(private smsGateway: SmsGateway) { }
 
-    sendSms(mobile:number, variables: object[]):Promise<any> {
+    sendSms(mobile: number, variables: object[]): Promise<any> {
         return this.smsGateway.send(mobile, variables);
     }
 }
 
-export default {
-    SmsSender,
-    TwoFactor,
-    Msg91
-}
+export { TwoFactor } from './2factor/2factor';
+export { Msg91 } from './msg91/msg91';
